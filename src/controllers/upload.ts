e8bca933e61cb9dbf103f14be90d31d28b1929ee
Adam Cardenas <cevaris@gmail.com>
2020-03-08 16:06:24 -0600

    adding types

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import fileUpload from 'express-fileupload';
 
 module.exports = (app: express.Express) => {
-    app.post('/upload', (req: any, res) => {
+    app.post('/upload', (req: express.Request, res: express.Response) => {
         try {
             if (!req.files) {
                 res.send({
@@ -10,8 +11,8 @@ module.exports = (app: express.Express) => {
                 });
             } else {
                 //Use the name of the input field (i.e. "file") to retrieve the uploaded file
-                let file = req.files.file;
-
+                const file: fileUpload.UploadedFile = req.files.file;
+                
                 //Use the mv() method to place the file in upload directory (i.e. "uploads")
                 file.mv('./public/' + file.name);
 
