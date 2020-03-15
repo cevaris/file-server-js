221a243a25c27148faa30713823488698f9ef4b4
Adam Cardenas <cevaris@gmail.com>
2020-03-15 15:02:09 -0600

    added integ tests

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import fileUpload from 'express-fileupload';
 var app = express();
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload({
     createParentPath: true,
--- tests/index.integration.ts
+++ tests/index.integration.ts
@@ -1,5 +1,7 @@
+import fs from 'fs';
 import http from 'http';
 import request from 'supertest';
+import { v4 } from 'uuid';
 
 process.env.PORT = '3333'; // choose another port for integ tests
 const server: http.Server = require('../index');
@@ -10,7 +12,7 @@ afterAll(() => {
 
 describe('file-server', () => {
     it('is running', async () => {
-        return await request(server)
+        await request(server)
             .get('/')
             .then((resp) => {
                 expect(resp.status).toBe(200);
@@ -19,14 +21,33 @@ describe('file-server', () => {
     });
 
     it('can fetch file', async () => {
-        return await request(server)
-            .get('/files/clouds.jpg')
-            .then((resp) => {
-                expect(resp.status).toBe(200);
+        const resp = await request(server)
+            .get('/files/clouds.jpg');
 
-                const cloudsJpgByteLength = 149667;
-                expect(resp.body instanceof Buffer).toBe(true);
-                expect((resp.body as Buffer).length).toBe(cloudsJpgByteLength);
-            });
+        expect(resp.status).toBe(200);
+
+        const cloudsJpgByteLength = 149667;
+        expect(resp.body instanceof Buffer).toBe(true);
+        expect((resp.body as Buffer).length).toBe(cloudsJpgByteLength);
+    });
+
+    it('can upload a file', async () => {
+        const uuid = v4();
+        const filePath = `/tmp/test.txt`;
+        await fs.promises.writeFile(filePath, uuid, 'UTF-8');
+
+        const uploadResp = await request(server)
+            .post('/files/upload.json')
+            .attach('file', filePath)
+            .set('Accept', 'application/json')
+
+        expect(uploadResp.status).toBe(200);
+        expect(uploadResp.body.status).toBe(true);
+
+        const getResponse = await request(server)
+            .get('/files/test.txt');
+
+        expect(getResponse.status).toBe(200);
+        expect(getResponse.text).toBe(uuid);
     });
 });
